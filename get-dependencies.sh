9c24202d27e5fb08662a3b522190813989429070
juju4 <juju4@users.noreply.github.com>
2017-01-07 22:55:26 +0100

    Initial commit on github

---
+++ get-dependencies.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+## one script to be used by travis, jenkins, packer...
+
+umask 022
+
+if [ $# != 0 ]; then
+rolesdir=$1
+else
+rolesdir=$(dirname $0)/..
+fi
+
+#[ ! -d $rolesdir/juju4.redhat-epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat-epel
+## galaxy naming: kitchen fails to transfer symlink folder
+#[ ! -e $rolesdir/juju4.upgrade-pkgs-gem ] && ln -s ansible-upgrade-pkgs-gem $rolesdir/juju4.upgrade-pkgs-gem
+[ ! -e $rolesdir/juju4.upgrade-pkgs-gem ] && cp -R $rolesdir/ansible-upgrade-pkgs-gem $rolesdir/juju4.upgrade-pkgs-gem
+
+## don't stop build on this script return code
+true
+
